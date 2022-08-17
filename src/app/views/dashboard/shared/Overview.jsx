756e2b6f8a89a1e2de9d487b38baf52b2b08a845
Seun Odeyemi <seunodeyemi@Clevers-MacBook-Pro.local>
2022-08-17 15:12:25 +0100

    updated arike's code

--- src/app/views/dashboard/shared/Overview.jsx
+++ src/app/views/dashboard/shared/Overview.jsx
@@ -2,127 +2,47 @@ import React, { Component } from "react";
 import { Grid, Card, Icon, IconButton, Tooltip } from "@material-ui/core";
 import AdjustIcon from "@mui/icons-material/Adjust";
 import SalesStatistics from "./SalesStatistics";
+import OverviewCard from "./OverviewCard";
+
+const overviews = [
+  {
+    rate: "+24.4%",
+    period: "From Last Month",
+    amount: "#1,546,805,865.76",
+    label: "Total Sales",
+    type: "CREDIT"
+  },
+  {
+    rate: "-24.4%",
+    period: "From Last Month",
+    amount: "#1,546,805,865.76",
+    label: "Total Expenses",
+    type: "DEBIT"
+  },
+  {
+    rate: "+24.4%",
+    period: "From Last Month",
+    amount: "18,043",
+    label: "Total Visitors",
+    type: "CREDIT"
+  },
+  {
+    rate: "-24.4%",
+    period: "From Last Month",
+    amount: "23,654",
+    label: "Total Orders",
+    type: "DEBIT"
+  }
+]
 
 const Overview = ({ theme }) => {
   return (
-    <Card  className="play-card p-10 bg-paper mb-24">
+    <Card className="play-card p-10 bg-paper mb-24">
       <div className="card-title mb-12">Overview</div>
       <Grid container spacing={1}>
         <div class="scrolling-wrapper-flexbox">
-          <Grid item xs={12} md={4}>
-            <div class="scrollable-item">
-              <Card className="p-10 kard">
-                <div className="flex flex-middle">
-                  <AdjustIcon
-                    style={{
-                      fontSize: "25px",
-                      opacity: 0.6,
-                      color: theme.palette.primary.main,
-                    }}
-                  >
-                    group
-                  </AdjustIcon>
-                  <div className="ml-12">
-                    <small className="text-muted">
-                      <h6> +24.4% </h6>
-                      <p>From Last Month</p>
-                    </small>
-                  </div>
-                </div>
-                <h6 className="m-0 mt-4 text-primary font-weight-500">
-                  #1,546,805,865.76
-                </h6>
-                <p>Total Sales</p>
-              </Card>
-            </div>
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <div class="scrollable-item">
-              <Card className="p-10 kard">
-                <div className="flex flex-middle">
-                  <AdjustIcon
-                    style={{
-                      fontSize: "25px",
-                      opacity: 0.6,
-                      color: theme.palette.primary.main,
-                    }}
-                  >
-                    attach_money
-                  </AdjustIcon>
-                  <div className="ml-12">
-                    <small className="text-muted">
-                      <h6> -24.4% </h6>
-                      <p>From Last Month</p>
-                    </small>
-                  </div>
-                </div>
-                <h6 className="m-0 mt-4 text-primary font-weight-500">
-                  #1,546,805,865.76
-                </h6>
-                <p>Total Expenses</p>
-              </Card>
-            </div>
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <div class="scrollable-item">
-              <Card className="p-10 kard">
-                <div className="flex flex-middle">
-                  <AdjustIcon
-                    style={{
-                      fontSize: "25px",
-                      opacity: 0.6,
-                      color: theme.palette.primary.main,
-                    }}
-                  >
-                    group
-                  </AdjustIcon>
-                  <div className="ml-12">
-                    <small className="text-muted">
-                      <h6> +24.4% </h6>
-                      <p>From Last Month</p>
-                    </small>
-                  </div>
-                </div>
-                <h6 className="m-0 mt-4 text-primary font-weight-500">18,043</h6>
-                <p>Total Visitors</p>
-              </Card>
-            </div>
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <div class="scrollable-item">
-              <Card className="p-10 kard">
-                <div className="flex flex-middle">
-                  <AdjustIcon
-                    style={{
-                      fontSize: "25px",
-                      opacity: 0.6,
-                      color: theme.palette.primary.main,
-                    }}
-                  >
-                    attach_money
-                  </AdjustIcon>
-                  <div className="ml-12">
-                    <small className="text-muted">
-                      <h6>-24.4%</h6>
-                      <p>From Last Month</p>
-                    </small>
-                  </div>
-                </div>
-                <h6 className="m-0 mt-4 text-primary font-weight-500">23,654</h6>
-                <p>Total Orders</p>
-                <Tooltip title="View Details" placement="top">
-                  <IconButton>
-                    <Icon>arrow_right_alt</Icon>
-                  </IconButton>
-                </Tooltip>
-              </Card>
-            </div>
-          </Grid>
+          { overviews.map(overview => <OverviewCard key={overview.label} overview={overview} theme={theme} />) }
         </div>
-        
         <Grid item xs={12} md={12}>
           <SalesStatistics />
         </Grid>
---
+++ src/app/views/dashboard/shared/OverviewCard.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Grid, Card, Icon, IconButton, Tooltip } from "@material-ui/core";
+import AdjustIcon from "@mui/icons-material/Adjust";
+import SalesStatistics from "./SalesStatistics";
+
+const OverviewCard = ({ theme, overview }) => {
+    return <Grid item xs={12} md={4}>
+        <div class="scrollable-item">
+            <Card className="p-10 kard">
+                <div className="flex flex-middle">
+                    <AdjustIcon
+                        style={{
+                            fontSize: "25px",
+                            opacity: 0.6,
+                            color: theme.palette.primary.main,
+                        }}
+                    >
+                        group
+                    </AdjustIcon>
+                    <div className="ml-12">
+                        <small className="text-muted">
+                            <h6> {overview?.rate} </h6>
+                            <p>{overview?.period}</p>
+                        </small>
+                    </div>
+                </div>
+                <h6 className="m-0 mt-4 text-primary font-weight-500">
+                    {overview?.amount}
+                </h6>
+                <p>{overview?.label}</p>
+            </Card>
+        </div>
+    </Grid>
+}
+
+export default OverviewCard;
