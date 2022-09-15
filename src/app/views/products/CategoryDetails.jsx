0da89a7f198702cc80e4a9bb49482e871611487a
Seun Odeyemi <seunodeyemi@Clevers-MacBook-Pro.local>
2022-09-15 12:53:57 +0100

    categories modification done

--- src/app/views/products/CategoryDetails.jsx
+++ src/app/views/products/CategoryDetails.jsx
@@ -1,70 +1,131 @@
 import React, { useEffect } from 'react'
-import { Grid } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import './product-details.css'
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import CategoryEdit from './components/CategoryEdit'
-import CategoryStatus from './components/CategoryStatus'
-import CategoryImage from './components/CategoryImage'
-import CategorySelect from './components/CategorySelect';
 import "./common.css"
+import {
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Box,
+  Grid,
+} from '@material-ui/core'
+import { SimpleCard, Breadcrumb } from 'matx'
+import Loading from 'matx/components/MatxLoadable/Loading'
+import http from '../../services/api';
+import { useState } from 'react';
+import ProductSubcategories from './components/ProductSubcategories'
+import CategoryProducts from './components/CategoryProducts'
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-
-  color: theme.palette.text.secondary,
-}));
-
-const CategoryDetails = ({ location, placeholder }) => {
+const CategoryDetails = ({ location }) => {
+  const [loading, setLoading] = React.useState(false)
+  const [category, setCategory] = useState(null);
+  const [subCategories, setSubCategories] = useState(null);
+  const [categoryProducts, setCategoryProducts] = useState(null);
   const State = location.state
   const { id } = State
-  const getCategory = () => { }
+
+  const getCategory = () => {
+    setLoading(true);
+    http.get(`/afrimash/product-categories/${id}`).then((response) => {
+      setLoading(false);
+      let { data } = response;
+      setCategory(data?.object);
+      setSubCategories(data?.object?.subCategories);
+    }).catch(err => {
+      setLoading(false);
+    });
+  }
+
+  const getCategoryProducts = () => {
+    http.get(`/afrimash/product-categories/${id}/products`).then((response) => {
+      let { data } = response;
+      setCategoryProducts(data?.object);
+    }).catch(err => { });
+  }
+
 
   useEffect(() => {
-    getCategory()
+    getCategory();
+    getCategoryProducts();
   }, [])
 
   return (
     <div className='m-sm-30'>
       <div className='mb-sm-30'>
-        <Grid container spacing={2}>
-          <Grid container spacing={2} item xs={12}>
-            <Grid item xs={12}>
-              <div className='page-title'>Edit Category</div>
-            </Grid>
-          </Grid>
-
-          <Grid container spacing={2} item xs={8}>
-            <Grid item xs={12}>
-              <Item className='no-shadow'>
-                <CategoryEdit />
-              </Item>
-            </Grid>
-            <Grid item xs={12}>
-              <Grid item xs={12}>
-                <Item className='no-shadow'>
-                  <CategorySelect />
-                </Item>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid container spacing={2} item xs={4} style={{ display: 'initial' }}>
-            <Grid item xs={12}>
-              <Item className='no-shadow'>
-                <CategoryStatus />
-              </Item>
-            </Grid>
-            <Grid item xs={12}>
-              <Item className='no-shadow'>
-                <CategoryImage />
-              </Item>
-            </Grid>
-          </Grid>
-        </Grid>
+        <Breadcrumb
+          routeSegments={[
+            { name: 'Categories', path: '/product-categories' },
+            { name: 'Category' },
+          ]}
+        />
       </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <SimpleCard>
+            <div className='flex flex-space-between flex-middle'>
+              <h5 className='text-left'>Product Category Details</h5>
+              {/* <Button
+                variant='contained'
+                color='primary'
+              >
+                Edit Product Category
+              </Button> */}
+            </div>
+            <Divider className='mt-4' />
+            <Table className='mb-4'>
+              <TableBody>
+                <TableRow>
+                  <TableCell className='pl-4'>Category Name</TableCell>
+                  <TableCell>
+                    <h5 className='m-0'>{category?.name ?? "---"}</h5>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='pl-4'>Commission</TableCell>
+                  <TableCell>
+                    <div>{category?.commission ?? "---"}</div>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='pl-4'>Is Category Featured</TableCell>
+                  <TableCell>
+                    <div>{category?.isFeatured ? "Yes" : "No"}</div>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='pl-4'>Is Category Featured on USSD</TableCell>
+                  <TableCell>
+                    <div>{category?.isFeaturedOnUssd ? "Yes" : "No"}</div>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='pl-4'>Category Images</TableCell>
+                  <TableCell>
+                    <Grid container spacing={2} className='category-images'>
+                      {
+                        category?.productCategoryImages?.length ? category?.productCategoryImages?.map((img, idx) => <Grid key={idx + 'cat'} item lg={3} md={3} sm={6} xs={12}>
+                          <div>
+                            <img src={img?.imageUrl} />
+                          </div>
+                        </Grid>) : <div>No image</div>
+                      }
+                    </Grid>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </SimpleCard>
+          <Box mt={5}>
+            <ProductSubcategories subCategories={subCategories} />
+          </Box>
+          <Box mt={5}>
+            <CategoryProducts categoryProducts={categoryProducts} />
+          </Box>
+        </>
+      )}
     </div>
   )
 }
