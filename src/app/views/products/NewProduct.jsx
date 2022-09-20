9ea978ca7227abc7c1dad05ac82b4576bce97d6b
Seun Odeyemi <seunodeyemi@Clevers-MacBook-Pro.local>
2022-09-20 08:18:33 +0100

    updated product creation modal

--- src/app/views/products/NewProduct.jsx
+++ src/app/views/products/NewProduct.jsx
@@ -1,51 +1,40 @@
 import React from 'react'
-import { Breadcrumb, SimpleCard } from 'matx'
 import { makeStyles } from '@material-ui/core/styles'
 import ProductForm from './ProductForm'
-import { TextField, Modal, Button, Grid, MenuItem } from '@material-ui/core'
+import { Modal } from '@material-ui/core'
 
-function NewProduct({ location, isOpen, handleClose }) {
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
+function NewProduct({ location, isOpen, handleClose, created }) {
+  const useStyles = makeStyles((theme) => ({
+    paper: {
       position: 'absolute',
       width: 500,
       backgroundColor: theme.palette.background.paper,
       border: '1px solid #000',
       boxShadow: theme.shadows[2],
       // padding: theme.spacing(5, 4, 3),
-  },
-}))
+    },
+  }))
 
-const classes = useStyles()
+  const classes = useStyles()
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10
-}
-
-function getModalStyle() {
-
-  const top = 60 + rand()
-  const left = 50 + rand()
-
-  return {
-      top: `${top}%`,
-      left: `${left}%`,
-      transform: `translate(-${top}%, -${left}%)`,
-      overflowY: "scroll", 
+  function getModalStyle() {
+    return {
+      top: `50%`,
+      left: `50%`,
+      transform: `translate(-50%, -50%)`,
+      overflowY: "scroll",
       maxHeight: "90%"
+    }
   }
-}
 
-const [modalStyle] = React.useState(getModalStyle)
+  const [modalStyle] = React.useState(getModalStyle)
   return (
     <Modal open={isOpen} onClose={handleClose} >
       <div className={`${classes.paper}`} style={modalStyle} >
-        <ProductForm isNewProduct={true} />
-      </div>    
+        <ProductForm isNewProduct={true} created={created} closeModal={handleClose} />
+      </div>
     </Modal>
   )
 }
 
 export default NewProduct
-  
