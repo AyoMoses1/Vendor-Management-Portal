50f12a5eabfe57114612586d6592b70c42004927
Seun Odeyemi <seunodeyemi@Clevers-MacBook-Pro.local>
2022-09-23 13:47:58 +0100

    done with product API integration

--- src/app/views/products/components/ProductGallery.jsx
+++ src/app/views/products/components/ProductGallery.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import './shared.css'
 import Box from '@mui/material/Box';
-import { Button, Grid } from '@material-ui/core';
+import { Button, CircularProgress, Grid } from '@material-ui/core';
 import { Lightbox } from "react-modal-image";
+import { updateProduct } from '../ProductService';
+import Alert from 'app/components/Alert';
 
 const ProductGallery = ({ product }) => {
     const [displayImage, setDisplayImage] = useState(0);
@@ -10,12 +12,21 @@ const ProductGallery = ({ product }) => {
     const [image, setImage] = useState(null)
     const [preview, setPreview] = useState(null)
     const [boxState, setBoxState] = useState(false);
+    const [updating, setUpdating] = useState(false);
+    const [alertData, setAlertData] = useState({ success: false, text: '', title: '' });
+    const [isOpen, setIsOpen] = React.useState(false)
+    const [values, setValues] = React.useState();
+
+    const handleDisplayModal = () => {
+        setIsOpen(!isOpen)
+    }
 
     useEffect(() => {
         if (product) {
             setImages(product?.productImages)
+            setValues({ ...product })
         }
-    })
+    }, [product])
 
     const handleBoxState = () => {
         setBoxState(!boxState);
@@ -25,6 +36,32 @@ const ProductGallery = ({ product }) => {
         setImage(file.imageUrl);
         setPreview(file.id);
         handleBoxState();
+    }
+
+    const handleSubmit = () => {
+        setUpdating(true)
+        updateProduct({ ...values })
+            .then((res) => {
+                if (res.status === 200) {
+                    setValues(res?.data?.object);
+                    setImages(res?.data?.object?.productImages)
+                    setUpdating(false)
+                    setAlertData({ success: true, text: "Product updated sucessfully", title: 'Product Updated' })
+                    handleDisplayModal();
+                }
+                else {
+                    setUpdating(false)
+                    setAlertData({ success: false, text: 'Invalid details provided', title: 'Unable to update product' })
+                    handleDisplayModal();
+                };
+            })
+            .catch((err) => console.error(err));
+    };
+
+    const deleteImage = (id) => {
+        const newList = images?.filter(p => p?.id !== id)
+        setImages(newList);
+        setValues({ ...values, productImages: newList })
     }
 
     return <Box
@@ -38,7 +75,16 @@ const ProductGallery = ({ product }) => {
     >
         <div className='flex justify-between items-center'>
             <div className="product-details-title">Gallery</div>
-            <Button variant='contained' color='primary' className='product-outline-save-btn'>Save</Button>
+            <Button
+                variant='contained'
+                color='primary'
+                className='product-outline-save-btn'
+                onClick={handleSubmit}
+                disabled={updating}
+            >
+                {updating ? <CircularProgress size={14} className='mr-10' /> : ''}
+                {updating ? "Please wait..." : "Save"}
+            </Button>
         </div>
         <Grid container spacing={2} className='mt-4'>
             <Grid item lg={12} md={12} sm={12} xs={12}>
@@ -55,7 +101,7 @@ const ProductGallery = ({ product }) => {
                                         if (i !== displayImage)
                                             setDisplayImage(i)
                                     }} className={i === displayImage ? '' : 'action-item'}>{i === displayImage ? 'Display Photo' : 'Set as display'}</div>
-                                    <img src='/assets/icon/delete-basket.svg' />
+                                    <img onClick={() => deleteImage(_image?.id)} src='/assets/icon/delete-basket.svg' />
                                 </div>
                                 <div className='gallery-base'>
                                     <div className='action-item' onClick={() => handleSetImage(_image)}>View</div>
@@ -113,6 +159,12 @@ const ProductGallery = ({ product }) => {
                 </div>
             </Grid> */}
         </Grid>
+        <Alert
+            isOpen={isOpen}
+            handleModal={handleDisplayModal}
+            alertData={alertData}
+            handleOK={handleDisplayModal}
+        />
     </Box>
 }
 
