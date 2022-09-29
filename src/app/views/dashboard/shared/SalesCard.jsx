c6d7aedab95f30202df66708464ec3939abf2b61
arike <arike@afrimash.com>
2022-09-29 09:28:41 +0100

    updates

--- src/app/views/dashboard/shared/SalesCard.jsx
+++ src/app/views/dashboard/shared/SalesCard.jsx
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import { Grid, Card, Icon, IconButton, Tooltip } from "@material-ui/core";
 
-
-const SalesCard = ({ theme, sales }) => {
-    return (
-        <div className="kird">
-            <div className="sales-card-data">
-                <h6 className="sales-card-label">
-                    {sales?.label}
-                </h6>
-                <h4 className="m-0 mt-4 text-primary font-weight-500 sales-card-amount">
-                    {sales?.amount}
-                </h4>
-            </div>
-            <div className="sales-funnel"></div>
-        </div>
-    );
-}
-
+const SalesCard = ({ theme, sales, maxValue }) => {
+  return (
+    <div className="kird">
+      <div className="sales-card-data">
+        <h6 className="sales-card-label">{sales?.label}</h6>
+        <h4 className="m-0 mt-4 text-primary font-weight-500 sales-card-amount">
+          {sales?.amount}
+        </h4>
+      </div>
+      <div className="sales-funnel" style={{clipPath: "polygon(0 1%, 100% 16%, 100% 85%, 0% 100%)"}}>
+        
+      </div>
+    </div>
+  );
+};
 
 export default SalesCard;
